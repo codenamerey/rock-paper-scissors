d9084045ad4f084cabdace777a3dcbc77a5c75a0
Lee Ryan <2solimanleeryan@gmail.com>
2022-09-04 13:04:20 +0800

    Establish logic of winner and loser

--- src/script.js
+++ src/script.js
@@ -3,3 +3,30 @@ function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     return choices[choice];
 }
+
+function playOneRound(playerSelection,  computerSelection) {
+    let result;
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    computerSelection = getComputerChoice();
+    if(playerSelection == computerSelection) {
+        result = "It's a tie!";
+    }
+
+    else if (
+        (computerSelection == 'Rock' && playerSelection == 'Scissors') ||
+        (computerSelection == 'Paper' && playerSelection == 'Rock') ||
+        (computerSelection == 'Scissors' && playerSelection == 'Paper')
+        )
+
+        {
+            result = `${computerSelection} beats ${playerSelection}! Too bad!`;
+        }
+
+    else {
+        result = `You win! ${playerSelection} beats ${computerSelection}!`
+    }
+    
+    return result;
+}
+
+console.log(playOneRound('Rock'));
